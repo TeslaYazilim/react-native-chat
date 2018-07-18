ef3b12b306195872fe95daa2996abfd8cd9e784d
Cagdas <cagdaskorkut@gmail.com>
2018-07-18 13:14:28 +0300

    denemeler

--- ChatProject/App.js
+++ ChatProject/App.js
@@ -1,23 +1,58 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, AppRegistry, TextInput } from 'react-native';
 
-export default class App extends React.Component {
+export default class App extends Component {
+  constructor() {
+    super();
+    this.state={
+      text: "deneme"
+    };
+  }
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
-    );
+        <View style={styles.chatMessages}>
+          <Text>{this.state.text}</Text>
+        </View>
+        <View style={styles.writeMessage}>
+          <TextInput
+            style={styles.writeMessage_input}
+            // onChangeText={(text) => this.setState({text})}
+            onSubmitEditing={(event) => this.setState({text: event.nativeEvent.text})} 
+            value={this.state.text}
+          />
+          
+        </View>
+      </View> 
+    ); 
   }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#0ff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  chatMessages: {
+    flex: 10,
+    backgroundColor: '#0ff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    alignSelf: 'stretch',
+  },
+  writeMessage: {
+    flex: 1,
+    backgroundColor: '#f00',
+    alignItems: 'center',
+    justifyContent: 'center',
+    alignSelf: 'stretch',
+  },
+  writeMessage_input: {
+    /*height: 40,
+    /*borderColor: 'gray',
+    borderWidth: 1,*/
+    width: '100%',
   },
 });
